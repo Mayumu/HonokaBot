bd881c7652955a977ce156968f09ce504aed64ab
Paweł Cyman <tazfelis@gmail.com>
2016-02-12 13:58:12 +0100

    Sample connection info class and changes to README

---
+++ HonokaBot/HonokaBot/SampleConnectionInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace HonokaBot
+{
+    class SampleConnectionInfo
+    {
+        //your twich login - must be lowercase
+        public static string twitchOwner = "xxxxxxx";
+        //your bot's twitch login - also must be lowercase
+        public static string twitchLogin = "honokabot";
+        //your bot's twitch password
+        public static string twitchPass = "oauth:xxxxxxxxxxxxxxxxxxxxx";
+        //your mysql database
+        public static string mysqlLogin = "Server=xxxxxxxxxxxxxx; database=xxxxxxx; UID=xxxxxxx; password=xxxxxxx";
+        //how to create table: CREATE TABLE `BotCommands` (`id` INT AUTO_INCREMENT PRIMARY KEY, `command` VARCHAR(20) NOT NULL, `reply` VARCHAR(250) NOT NULL)
+        //your osu API key
+        public static string osuAPIKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        //your osu login
+        public static string osuUsername = "xxxxxxx";
+        //your osu IRC password (it's different than your regular password!)
+        public static string osuIRCPassword = "xxxxxxx";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # HonokaBot
 Twitch chat bot with the ability to read osu! map links and sending them in-game.
+
+Current functionality:
+ - joining any channel on twitch.tv
+ - taking static commands and giving proper replies
+ - adding new commands with "!addcom !commandcall command reply"
+ - removing commands with "!delcom !commandcall"
+ - editing existing commands with "!editcom !commandcall new command reply"
+ - taking osu! beatmap links from chat, getting map info from the API and sending a private message to your osu! account
+
+To make it work you need to fill in all the fields in the "SampleConnectionInfo.cs" class and rename it to "ConnectionInfo.cs".
+It'll require you to have a Twitch.tv account as well as an osu! account.
+
+http://twitch.tv - Twitch, a gaming livestreaming platform
+http://osu.ppy.sh - osu!, a free rhythm game
